d12a6a7465cbfd9463bd39dcd8e556e2838dad4b
I Rahman <inamur.r@transneuron.com>
2019-10-15 12:45:04 +0530

    hour hand functionality

--- Js/script.js
+++ Js/script.js
@@ -1,21 +1,28 @@
 var secoundDegree = 270;
 var minuteDegree = 270;
 var hourDegree = 0;
-startMinute = (secoundDegree) => {
-  var secound = document.getElementById('minute').style.transform = "rotate(" + secoundDegree + "deg)";
- console.log(secound); 
+startMinute = (minuteDegree) => {
+  var secound = document.getElementById('minute').style.transform = "rotate(" + minuteDegree + "deg)";
 }
 startSecound = (secoundDegree) => {
   var secound = document.getElementById('secound').style.transform = "rotate(" + secoundDegree + "deg)";
- console.log(secound); 
+}
+startHour = (hourDegree) => {
+  var secound = document.getElementById('hour').style.transform = "rotate(" + hourDegree + "deg)";
 }
 
-
-setInterval( function() {
+startClock = () =>{
+  setInterval( function() {
   secoundDegree = secoundDegree + 6;
   startSecound(secoundDegree);
-}, 1000);
-setInterval( function() {
-  minuteDegree = minuteDegree + 6;
-  startMinute(minuteDegree);
-}, 60000);
+  }, 1000);
+  setInterval( function() {
+    minuteDegree = minuteDegree + 6;
+    startMinute(minuteDegree);
+  }, 60000);
+  setInterval( function() {
+    hourDegree = hourDegree + 30;
+    startHour(hourDegree);
+  }, 3600000);
+}
+startClock();
