30acb58dea79245e1f3eae29c07dd2b44a10f8be
I Rahman <inamur.r@transneuron.com>
2019-10-15 17:43:55 +0530

    hour hand percentage

--- Js/script.js
+++ Js/script.js
@@ -11,16 +11,14 @@ startHour = (hourDegree) => {
 startClock = () =>{
   setInterval( function() {
   secoundDegree = secoundDegree + 6;
+  if((secoundDegree) % 360 == 270){
+    minuteDegree = Number(minuteDegree) + Number(6);
+    hourDegree = parseFloat(hourDegree) + parseFloat(Number(0.5).toFixed(1));
+    startMinute(minuteDegree);
+    console.log(secoundDegree + ":" + minuteDegree + ":" + hourDegree);
+  }
   startSecound(secoundDegree);
   }, 1000);
-  setInterval( function() {
-    minuteDegree = minuteDegree + 6;
-    startMinute(minuteDegree);
-  }, 60000);
-  setInterval( function() {
-    hourDegree = hourDegree + 30;
-    startHour(hourDegree);
-  }, 3600000);
 }
 //Iniliza the Date and calulate each hands degree
 var time = new Date();
@@ -29,10 +27,15 @@ var minute = time.getMinutes();
 var hour = time.getHours();
 var secoundDegree = 270 + Number(secound*6);
 var minuteDegree = 270 + Number(minute*6);
-var hourDegree = 270 + Number((hour%12)*30);
+let subHour = (minuteDegree % 60)/2;
+var hourDegree = 270 + Number((hour%12)*30) + subHour;
 //Inilize with the currebt initial value
-document.getElementById('minute').style.transform = "rotate(" + minuteDegree + "deg)";
-document.getElementById('secound').style.transform = "rotate(" + secoundDegree + "deg)";
-document.getElementById('hour').style.transform = "rotate(" + hourDegree + "deg)";
+window.onload = function() {
+  console.log(secoundDegree + ":" + minuteDegree + ":" + hourDegree + ":" + subHour);
+  document.getElementById('clock').style.display='block';
+  document.getElementById('minute').style.transform = "rotate(" + minuteDegree + "deg)";
+  document.getElementById('secound').style.transform = "rotate(" + secoundDegree + "deg)";
+  document.getElementById('hour').style.transform = "rotate(" + hourDegree + "deg)";
+}
 //Start the Clock
 startClock();
